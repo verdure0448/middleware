bf4ceeefe89e9a61341f41ed712595d0cfc68488
동범 김 <dbkim@hd-bsnc.com>
2019-06-25 13:46:52 +0900

    기동 시 전체 삭제 정적 핸들러 추가

--- com.hdbsnc.smartiot.adapter.mb.mc.bin/src/com/hdbsnc/smartiot/adapter/mb/mc/bin/MitsubishiQSeriesMCAdapterInstance.java
+++ com.hdbsnc.smartiot.adapter.mb.mc.bin/src/com/hdbsnc/smartiot/adapter/mb/mc/bin/MitsubishiQSeriesMCAdapterInstance.java
@@ -63,6 +63,8 @@ public class MitsubishiQSeriesMCAdapterInstance implements IAdapterInstance {
 		root.putHandler("create/mb/melsec", new CreateRequestHandler("handler", 3000, sid, manager, aim, _log));
 		//멜섹 프로토콜 핸들러를 삭제한다.
 		root.putHandler("delete/mb/melsec", new DeleteRequestHandler("handler", 3000, sid, manager, aim, _log));
+		//멜섹 프로토콜 핸들러를 전체삭제한다.
+		root.putHandler("delete/all/mb/melsec", new DeleteRequestHandler("handler", 3000, sid, manager, aim, _log));
 		//멜섹 핸들러의 상태를 확인한다.
 		root.putHandler("status/mb/melsec", new RunningStatusCheckHandler("handler", 3000, manager, aim, _log));
 	}
