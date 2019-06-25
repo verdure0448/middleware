d73938d77622d8d355ee62fe866acbef324f9414
Unknown <verdure0448@gmail.com>
2019-06-25 15:20:48 +0900

    참조 변경 완료

--- com.hdbsnc.smartiot.adapter.zeromq/src/com/hdbsnc/smartiot/adapter/zeromq/processor/handler/RepHandler.java
+++ com.hdbsnc.smartiot.adapter.zeromq/src/com/hdbsnc/smartiot/adapter/zeromq/processor/handler/RepHandler.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.hdbsnc.smartiot.adapter.websocketapi.processor.handler.InnerContext;
+
 import com.hdbsnc.smartiot.adapter.zeromq.api.ZeromqApi;
 import com.hdbsnc.smartiot.adapter.zeromq.api.callback.RepCallback;
 import com.hdbsnc.smartiot.adapter.zeromq.obj.CommonRequest;
@@ -18,6 +18,7 @@ import com.hdbsnc.smartiot.common.aim.IAdapterInstanceManager;
 import com.hdbsnc.smartiot.common.context.IContext;
 import com.hdbsnc.smartiot.common.context.handler2.OutboundContext;
 import com.hdbsnc.smartiot.common.context.handler2.impl.AbstractTransactionTimeoutFunctionHandler;
+import com.hdbsnc.smartiot.common.context.impl.InnerContext;
 import com.hdbsnc.smartiot.util.logger.Log;
 
 /**
@@ -59,17 +60,17 @@ public class RepHandler extends AbstractTransactionTimeoutFunctionHandler {
 			// TODO 삭제 예정 [vo] -> [json string]
 			String sReq = gson.toJson(req);
 
-			ICtx.sid = inboundCtx.getSID(); // Device ID
-			ICtx.tid = "xxxxxx"; // Target ID
-			ICtx.paths = Arrays.asList("xxxxxx", "xxxxxxx");
+			ICtx.setSid(inboundCtx.getSID()); // Device ID
+			ICtx.setTid("xxxxxx"); // Target ID
+			ICtx.setPaths(Arrays.asList("xxxxxx", "xxxxxxx"));
 			break;
 		case "stop":
 			// 포멧 체크
 			gson.fromJson(content, StopRequest.class);
 
-			ICtx.sid = inboundCtx.getSID(); // Device ID
-			ICtx.tid = "xxxxxx"; // Target ID
-			ICtx.paths = Arrays.asList("xxxxxx", "xxxxxxx");
+			ICtx.setSid(inboundCtx.getSID()); // Device ID
+			ICtx.setTid("xxxxxx"); // Target ID
+			ICtx.setPaths(Arrays.asList("xxxxxx", "xxxxxxx"));
 			break;
 		case "stop.all":
 			// TODO
@@ -78,9 +79,9 @@ public class RepHandler extends AbstractTransactionTimeoutFunctionHandler {
 			// 포멧 체크
 			gson.fromJson(content, StatusRequest.class);
 
-			ICtx.sid = inboundCtx.getSID(); // Device ID
-			ICtx.tid = "xxxxxx"; // Target ID
-			ICtx.paths = Arrays.asList("xxxxxx", "xxxxxxx");
+			ICtx.setSid(inboundCtx.getSID()); // Device ID
+			ICtx.setTid("xxxxxx"); // Target ID
+			ICtx.setPaths(Arrays.asList("xxxxxx", "xxxxxxx"));
 			break;
 		default:
 			// 로그처리
@@ -91,7 +92,7 @@ public class RepHandler extends AbstractTransactionTimeoutFunctionHandler {
 
 		// 버프 포지션 초기화
 		inboundCtx.getContent().rewind();
-		ICtx.content = inboundCtx.getContent();
+		ICtx.setContent(inboundCtx.getContent());
 
 		try {
 			aim.handOverContext(ICtx, new RepCallback(zmqApi, log));
